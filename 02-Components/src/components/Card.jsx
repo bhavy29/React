d13c35ac01e526b78702dd2bb84db168b076afb0
Bhavy Chaudhary <=>
2025-12-30 11:28:01 +0530

    add Components

---
+++ 02-Components/src/components/Card.jsx
@@ -0,0 +1,8 @@
+function Card(){
+    return <div className="card">
+        <h1>Bhavy Chaudhary</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, tempore qui! Adipisci dolorum ipsam harum nam aliquam excepturi saepe esse provident quia quaerat distinctio deserunt odio, commodi quam quisquam veritatis?</p>
+    </div>
+}
+
+export default Card
